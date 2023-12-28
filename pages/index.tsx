b08b4b88ba7774658b33b6109722e0cfc3783e60
Ao Wang <wa18743786616@163.com>
2023-12-28 21:48:35 +0800

    Update index.tsx

--- pages/index.tsx
+++ pages/index.tsx
@@ -11,6 +11,9 @@ export default function Home() {
     // clientId="758c04f9bf0da56dc07ff4c7e6d18b83"
     supportedChains={[Ethereum, Goerli, Binance, Arbitrum]}
     supportedWallets={[
+      metamaskWallet(),
+      walletConnect(),
+      coinbaseWallet(),
       safeWallet({
         recommended: true,
         personalWallets: [metamaskWallet(), walletConnect(), coinbaseWallet()]
