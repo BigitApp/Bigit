1b426ebd2c45f887356ebcf8fdca55862f017ab3
Ao Wang <wa18743786616@163.com>
2024-01-04 20:48:36 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,7 @@ import { Hero } from './components/Hero';
 
 export default function App() {
   useEffect(() => {
-    document.title = "Bigit";
+    document.title = "Bigit丨全球最大的AI助手数字藏品生产、使用、交易平台";
   }, []);
 
   return (
