f73509531dc32a026c30f1d926b4f783f0b8688f
Ao Wang <wa18743786616@163.com>
2024-01-07 10:53:09 +0800

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -6,7 +6,6 @@ import Features from "@/app/components/home/Features/Features";
 export default function App() {
   useEffect(() => {
     document.title = "Bigit: Blockchain And AI Generate Your Individual Assistant";
-    document.body.style.transform = "scale(1.1)";
   }, []);
 
   return (
