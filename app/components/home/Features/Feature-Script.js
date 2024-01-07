22a67a1acccf5894f754d456573c14dc4e641379
Ao Wang <wa18743786616@163.com>
2024-01-07 22:16:36 +0800

    svg

--- app/components/home/Features/Feature-Script.js
+++ app/components/home/Features/Feature-Script.js
@@ -3,7 +3,7 @@ const features = [
     heading: "CREATE",
     link: "Go to Create",
     url: "/myBot",
-    image: "/home/app-mint.svg",
+    image: "/home/create.svg",
     title: "Create and Use Your AI Assistant",
     description:
       "Set the appearance, prompts, and knowledge (PDF, txt) of your AI assistant to better meet your needs (without any coding knowledge)",
@@ -12,7 +12,7 @@ const features = [
     heading: "MARKETPLACE",
     link: "Buy and List AI Assistant NFTs",
     url: "/market",
-    image: "/home/app-explore.svg",
+    image: "/home/market.svg",
     title: "Create Your AI Assistant, for Earning Cryptocurrency Revenue",
     description:
       "Our marketplace is where buyers and collectors trade AI Assistant NFTs effortlessly across the hottest blockchains",
