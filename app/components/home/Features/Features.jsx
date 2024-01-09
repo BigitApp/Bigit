5ae3ce76078c8a21b70456d813abb4cbf8a840eb
Ao Wang <wa18743786616@163.com>
2024-01-09 23:30:19 +0800

    Update Features.jsx

--- app/components/home/Features/Features.jsx
+++ app/components/home/Features/Features.jsx
@@ -11,7 +11,7 @@ const Features = () => {
     window.addEventListener("scroll", () => {
       for (const card of cards) {
         const cardTop = card.getBoundingClientRect().top;
-        if (cardTop + offSet <= window.innerHeight-120) {
+        if (cardTop + offSet <= window.innerHeight-200) {
           card.children[0].style.transform = "translateX(0)";
           card.children[1].style.transform = "translateX(0)";
         } else {
