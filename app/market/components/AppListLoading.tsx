893db9c8433b42bb419a1c1a9c296f6c5c921164
Ao Wang <wa18743786616@163.com>
2024-01-16 19:00:31 +0800

    Update AppListLoading.tsx

--- app/market/components/AppListLoading.tsx
+++ app/market/components/AppListLoading.tsx
@@ -36,7 +36,7 @@ const AppListLoading = () => {
       {[1, 2, 3, 4, 5, 6].map((app, idx) => (
         <li
           key={idx}
-          className="col-span-1 flex flex-col justify-between divide-y divide-gray-600 rounded-lg bg-white text-center shadow border border-gray-600"
+          className="col-span-1 flex flex-col justify-between divide-y rounded-lg text-center shadow-lg border bg-opacity-20 cursor-pointer transform hover:scale-105 transition-transform duration-200"
         >
           <div>
             <div className="flex flex-1 flex-col items-center p-8">
@@ -56,9 +56,9 @@ const AppListLoading = () => {
           </div>
 
           <div>
-            <div className="-mt-px flex divide-x divide-gray-600">
+            <div className="-mt-px flex divide-x">
               <div className="flex w-0 flex-1">
-                <div className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg border border-transparent py-4 text-sm font-semibold text-gray-900 cursor-pointer transform hover:scale-110 transition-transform duration-200" onClick={handleBuy}>
+                <div className="relative -mr-px inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-bl-lg  py-4 text-sm font-semibold text-gray-900 cursor-pointer transform hover:scale-110 transition-transform duration-200" onClick={handleBuy}>
                   <FireIcon
                     className="h-5 w-5 text-green-700"
                     aria-hidden="true"
@@ -67,7 +67,7 @@ const AppListLoading = () => {
                 </div>
               </div>
               <div className="-ml-px flex w-0 flex-1">
-                <div className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg border border-transparent py-4 text-sm font-semibold text-gray-900 cursor-pointer transform hover:scale-110 transition-transform duration-200" onClick={handleRun}>
+                <div className="relative inline-flex w-0 flex-1 items-center justify-center gap-x-3 rounded-br-lg py-4 text-sm font-semibold text-gray-900 cursor-pointer transform hover:scale-110 transition-transform duration-200" onClick={handleRun}>
                     <PlayIcon
                       className="h-5 w-5 text-green-700"
                       aria-hidden="true"
